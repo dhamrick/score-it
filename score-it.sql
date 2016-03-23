20974c51a8023ad2fa9d65d960ccdf4e044723de
Donovan K. Hamrick <donovan.hamrick@gmail.com>
2016-03-22 21:51:14 -0700

    Add several tables for data tracking.
    Adds the following basic tables: games, game_ownership, game_nights,
    and played_games, and their basic structures.

--- score-it.sql
+++ score-it.sql
@@ -1,15 +1,71 @@
-CREATE DATABASE score_it
+DROP DATABASE IF EXISTS score_it;
+
+\c score_it
+
+CREATE DATABASE score_it;
 
 CREATE TABLE players
-    -- Creates a table called players to hold the player's name and
-    -- whether or not they are currently active.
+    -- Holds all of the registered players.
     (
         player_id serial PRIMARY KEY,
-        name text,
+        user_name text,
         email text,
-        active text DEFAULT 'yes',
-        UNIQUE (email)
+        name text,
+        UNIQUE (email, user_name)
     );
+
+CREATE TABLE games
+    -- Holds all of the games played and information about them.
+    (
+        game_id serial PRIMARY KEY,
+        game_name text,
+        game_type text,
+        max_players integer,
+        min_players integer,
+        difficulty text
+     );
+
+CREATE TABLE game_ownership
+    -- Holds a record of who owns what games.
+    (
+        game_id serial REFERENCES games(game_id),
+        player_id serial REFERENCES players(player_id)
+    );
+
+CREATE TABLE game_nights
+    -- Holds all of the game nights and who played
+    (
+        game_night_id serial PRIMARY KEY,
+        play_date timestamp DEFAULT current_timestamp,
+        host serial REFERENCES players(player_id)
+    );
+
+CREATE TABLE played_games
+    -- Holds which players played in which game
+    (
+        game_night_id serial REFERENCES game_nights(game_night_id),
+        game_name text,
+        player_id serial REFERENCES players(player_id)
+
+    );
+
+
+-- CREATE VIEW current_tournament AS
+--     SELECT max(tournament_id) FROM tournaments;
+--     -- Creates a view for the current (latest) tournament_id
+
+-- CREATE VIEW last_registered_player AS
+--     SELECT max(player_id) FROM players;
+
+-- CREATE VIEW player_standings AS
+--     SELECT players.player_id, players.name, tournament_data.wins, tournament_data.matches
+--     FROM players JOIN tournament_data
+--     ON (players.player_id = tournament_data.player_id);
+
+
+
+
+
 
 
 
