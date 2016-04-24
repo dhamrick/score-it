8cf562346b130be4a691bcf4a67c50f1134f0566
Donovan K. Hamrick <donovan.hamrick@gmail.com>
2016-04-23 22:37:46 -0700

    Fix typos and connect to postgres database

--- database_setup.py
+++ database_setup.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Numeric
+from sqlalchemy import Column, ForeignKey, Integer, String, Numeric, Date
 from sqlalchemy import func
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql.expression import asc, desc
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
@@ -9,7 +10,7 @@ Base = declarative_base()
 class Players (Base):
     __tablename__ = 'players'
     id = Column(Integer, primary_key = True)
-    user_name = Column(String(15), nullable = False, unique = True) )
+    user_name = Column(String(15), nullable = False, unique = True)
     name = Column(String(25), nullable = False)
     email = Column(String(25), nullable = False, unique = True)
     address = Column(String(80), nullable = True)
@@ -25,8 +26,8 @@ class Games(Base):
 
 class GameOwnership (Base):
     __tablename__ = 'game_ownership'
-    id_player = Column(Integer, ForeignKey('players.id'))
-    id_game = Column(Integer, ForeignKey('games.id'))
+    id_player = Column(Integer, ForeignKey('players.id'), primary_key = True)
+    id_game = Column(Integer, ForeignKey('games.id'), primary_key = True)
 
 class GameNights (Base):
     __tablename__ = 'game_nights'
@@ -35,11 +36,16 @@ class GameNights (Base):
     host = Column(Integer, ForeignKey('players.id'))
 
 class GameStats (Base):
+    __tablename__ = 'game_stats'
     id = Column(Integer, primary_key = True)
-    game_night = Column(Integer, ForeignKey = 'game_nights.id')
-    id_game = Column(Integer, ForeignKey = 'games.id')
-    player = Column(Integer, ForeignKey = 'players.id')
+    game_night = Column(Integer, ForeignKey('game_nights.id'))
+    id_game = Column(Integer, ForeignKey('games.id'))
+    player = Column(Integer, ForeignKey('players.id'))
     score = Column(Integer, nullable = False)
+
+engine = create_engine('postgres://vagrant@/scoreit')
+
+Base.metadata.create_all(engine)
 
 
 
